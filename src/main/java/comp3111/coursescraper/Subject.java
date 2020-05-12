c2fd65ad4a29816a8a67b94e2ccaee825abb1ece
dsdaluwatte <dsdaluwatte@connect.ust.hk>
2020-05-12 21:04:03 +0530

    Subject class

---
+++ src/main/java/comp3111/coursescraper/Subject.java
@@ -0,0 +1,36 @@
+package comp3111.coursescraper;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Subject {
+	private String title ;
+	private List<Courses> courses;
+
+	public Subject () {
+		courses = new ArrayList<Courses>();
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void add_course(Course c){
+		this.courses.add(c);
+}
+
+	/**
+	* @return the sections
+	*/
+	public List<Course> get_courses(){
+		return this.courses;
+	}
+}
